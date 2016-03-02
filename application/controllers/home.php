5dbec93f877180ef332720983550a57aa813a476
carlo <carlorcarrascal@yahoo.com>
2016-03-03 06:07:16 +0800

    add theme

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -17,6 +17,23 @@ class Home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('url');
+
+		$this->_init();
+	}
+
+	private function _init()
+	{
+		$this->output->set_template('default');
+
+		$this->load->js('assets/themes/default/js/jquery-1.9.1.min.js');
+		$this->load->js('assets/themes/default/hero_files/bootstrap-transition.js');
+		$this->load->js('assets/themes/default/hero_files/bootstrap-collapse.js');
+	}
 	public function index()
 	{
 		$this->load->view('home_view');
